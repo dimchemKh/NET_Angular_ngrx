45c526d210d04e20f62f1fcdd1d61fd5a04dcded
dimchem.kh <dimchem.kh@gmail.com>
2020-07-21 12:04:45 +0300

    setup project

--- NET_Angular.API/Program.cs
+++ NET_Angular.API/Program.cs
@@ -7,7 +7,21 @@ namespace NET_Angular_Aionys
     {
         public static void Main(string[] args)
         {
+            //Task.Run(() =>
+            //{
+            //    var assembly = Assembly.LoadFrom("NET_Angular.WEB");
+
+            //    Type t = assembly.GetType("NET_Angular.WEB.Program");
+
+            //    var method = t.GetMethod("Main");
+
+            //    var obj = Activator.CreateInstance(t);
+            //    System.String[] args = new string[] { "%LAUNCHER_ARGS%" };
+            //    method.Invoke(obj, args);
+            //    //t.GetMethod("Main", new Type[], { new string[] { "%LAUNCHER_ARGS%" } });
+            //});
             CreateHostBuilder(args).Build().Run();
+
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
--- NET_Angular.WEB/Program.cs
+++ NET_Angular.WEB/Program.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace NET_Angular_ngrx
+namespace NET_Angular.WEB
 {
     public class Program
     {
